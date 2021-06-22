a793567997b3500de8e248a2b4237ac2a0ac328d
Domehoff <domhof@live.com>
2021-06-22 17:43:30 -0400

    Update hazard.rs

--- src/characters/hazard.rs
+++ src/characters/hazard.rs
@@ -18,7 +18,7 @@ pub struct Hazard {
 }
 
 impl Hazard {
-	pub fn new(c: Hazards) -> Stalactites {
+	pub fn new(c: Hazards) -> Hazard {
 		Hazard {
             active: false,
             falling: false,
@@ -30,57 +30,22 @@ impl Hazard {
     }
 
         // setters
-        pub fn set_active(&mut self, b:bool ) -> bool {
-            self.active = b;
-            return true;
-        }
-        pub fn set_falling(&mut self, b:bool) -> bool {
-            self.falling = b;
-            return true;
-        }
-        pub fn set_hit(&mut self, b:bool) -> bool {
-            self.hit = b;
-            return true;
-        }
-        pub fn set_fallspeed(&mut self, i:f64) -> bool {
-            self.fall_speed = i;
-            return true;
-        }
-        pub fn set_damage(&mut self, i:f64) -> bool {
-            self.damage = i;
-            return true;
-        }
-        pub fn set_xcord(&mut self, i:f64 ) -> bool {
-            self.x_cord = i;
-            return true;
-        }
-        pub fn set_ycord(&mut self, i:f64 ) -> bool {
-            self.y_cord = i;
-            return true;
-        }
+        pub fn set_active(&mut self, b:bool ) -> bool { self.active = b; }
+        pub fn set_falling(&mut self, b:bool) { self.falling = b; }
+        pub fn set_hit(&mut self, b:bool) { self.hit = b; }
+        pub fn set_fallspeed(&mut self, i:f64) { self.fall_speed = i; }
+        pub fn set_damage(&mut self, i:f64) { self.damage = i; }
+        pub fn set_xcord(&mut self, i:f64 ) { self.x_cord = i; }
+        pub fn set_ycord(&mut self, i:f64 ) { self.y_cord = i; }
 
         // getters
-        pub fn active(&self) -> bool {
-            self.active
-        }
-        pub fn falling(&self) -> bool {
-            self.falling
-        }
-        pub fn hit(&self) -> bool {
-            self.hit
-        }
-        pub fn fallspeed(&self) -> f64 {
-            self.fall_speed
-        }
-        pub fn damage(&self) -> f64 {
-            self.damage
-        }
-        pub fn xcord(&self) -> &Point {
-            self.position.x()
-        }
-        pub fn ycord(&self) -> &Point {
-            self.position.y()
-        }
+        pub fn active(&self) -> bool { self.active }
+        pub fn falling(&self) -> bool { self.falling }
+        pub fn hit(&self) -> bool { self.hit }
+        pub fn fallspeed(&self) -> f64 { self.fall_speed }
+        pub fn damage(&self) -> f64 { self.damage }
+        pub fn xcord(&self) -> &Point { self.position.x() }
+        pub fn ycord(&self) -> &Point { self.position.y() }
 
         // WIP
         pub fn fall(&mut self) -> bool {
